3bc46de1f95a77045fd168eccfa8983b534a5c8f
Tyler Rutschman <tyler.rutschman@garmin.com>
2015-05-05 23:04:46 -0500

    Script working and scaling photos

---
+++ Rakefile
@@ -0,0 +1,33 @@
+
+require 'fileutils'
+require 'bundler'
+Bundler.require(:default)
+
+Dir.glob(['lib/*.rb']).each { |r| load r}
+
+desc "Scale down Photos"
+task :scale do
+	photo_dir = 'test-photos/full'
+	# FNM_CASEFOLD is to allow for case-insensitive matching
+	photos = Dir.glob("#{photo_dir}/**/*.jpg", File::FNM_CASEFOLD)
+	progressbar = ProgressBar.create(
+		:title => "Scaling Photos",
+		:starting_at => 0,
+		:total => photos.length,
+		:format => '%a %bᗧ%i %c of %C (%p%%) %t',
+		:progress_mark  => ' ',
+		:remainder_mark => '･')
+	photos.each do |photo|
+		progressbar.log "Current: #{photo}"
+		Helpers.make_thumbnail(photo)
+		Helpers.make_scale(photo)
+	  progressbar.increment
+	end
+end
+
+desc "Create JSON Manifest"
+task :marshall do
+
+end
+
+
---
+++ lib/helpers.rb
@@ -0,0 +1,22 @@
+module Helpers
+	def self.make_thumbnail(filename)
+		directory = filename.split("/").first
+		img = Magick::Image::read(filename).first
+		thumbnail = img.scale(0.09)
+		thumbnail_filename = filename.gsub /#{directory}/, "#{directory}/thumbnails"
+		save_file(thumbnail_filename, thumbnail)
+	end
+	def self.make_scale(filename)
+		directory = filename.split("/").first
+		img = Magick::Image::read(filename).first
+		scaled = img.scale(0.4)
+		scaled_filename = filename.gsub /#{directory}/, "#{directory}/scaled"
+		save_file(scaled_filename, scaled)
+	end
+	def self.save_file(filename, image)
+		# Check if Directory exists and create if it doesn't
+		directory = File.dirname(filename)
+		FileUtils.mkdir_p(directory) unless Dir.exists?(directory) 
+		image.write filename
+	end
+end
