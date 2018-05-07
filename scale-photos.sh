22335874e472efce5a2a521a5dd576cc5cce4380
Tyler Rutschman <tyler.rutschman@garmin.com>
2018-05-07 16:19:22 -0500

    Update scale photos script to use latest

--- scale-photos.sh
+++ scale-photos.sh
@@ -1,7 +1,7 @@
 /bin/bash 
 
 docker run -it \
-	-v `pwd`:/srv outpacingmelanoma/photos:0.0.1-0 \
+	-v `pwd`:/srv outpacingmelanoma/photos:latest \
 	--name 'outpacing-photos-2018' \
 	--net=host \
 	scale['2018']
