6464da3d53c401c96b963af46de395df96b08750
Tyler Rutschman <tyler.rutschman@garmin.com>
2018-05-07 21:05:47 -0500

    Add more lines to start script

--- scale-photos.sh
+++ scale-photos.sh
@@ -2,6 +2,8 @@
 
 docker run -it \
 	-v `pwd`:/srv outpacingmelanoma/photos:latest \
+	-v /home/rutschman/Dropbox/2018-outpacing-photos:/photos \
+	outpacingmelanoma/photos:latest \
 	--name 'outpacing-photos-2018' \
 	--net=host \
 	scale['2018']
